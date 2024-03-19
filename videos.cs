e03227f94af11ebc9f70fa380299eb8546bbacaa
Sergio Ll <44053413+Rebel1898@users.noreply.github.com>
2024-03-19 13:42:52 +0100

    Add files via upload
    V.1.0.1  Corregidos errores menores en formato de los html

---
+++ videos.cs
@@ -0,0 +1,29 @@
+﻿using AngleSharp.Dom;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sacar_Hilos_Fino
+{
+    public class Resultado
+    {
+        public string titulo { get; set; }
+        public string URL { get; set; }
+
+        public string tipo { get; set; } = "POST";
+
+
+
+        public Resultado(string title, string link, string kind = "POST")
+        {
+            titulo = title;
+            URL = link;
+            tipo = kind;
+
+        }
+    }
+}
